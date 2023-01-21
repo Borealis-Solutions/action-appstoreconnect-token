d642ca28ad14364b427c7d05ff56af47f3353a6b
Dan Murfin <dan@dsmurfin.co.uk>
2023-01-21 15:31:02 +0000

    Add scope

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ const core = require('@actions/core')
 const keyId = process.env.INPUT_KEY_ID
 const issuerId = process.env.INPUT_ISSUER_ID
 const key = process.env.INPUT_KEY
+const scope = process.env.INPUT_SCOPE
 
 const now = Math.floor(Date.now() / 1000)
 
-const payload = { iss: issuerId, iat: now, aud: 'appstoreconnect-v1', exp: now + 20 * 60, aud: 'appstoreconnect-v1' }
+let payload = { iss: issuerId, iat: now, aud: 'appstoreconnect-v1', exp: now + 20 * 60 }
+if (scope == '') {
+    payload['scope'] = scope
+}
 const signedToken = jwt.sign(payload, key, { algorithm: 'ES256', keyid: keyId })
 
 core.setOutput('token', signedToken)
